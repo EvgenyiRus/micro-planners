78b59113cfe9809534debb274bf0fe874c09c448
Evgenyi Andreev <djonni-ussr@mail.ru>
2022-03-12 16:41:22 +0300

    feat: add priority

---
+++ planner-todo/src/main/java/com/example/micro/planner/todo/service/PriorityService.java
@@ -0,0 +1,45 @@
+package com.example.micro.planner.todo.service;
+
+import com.example.micro.planner.entity.Priority;
+import com.example.micro.planner.todo.repo.PriorityRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+// все методы класса должны выполниться без ошибки, чтобы транзакция завершилась
+// если в методе возникнет исключение - все выполненные операции из данного метода откатятся (Rollback)
+@Transactional
+public class PriorityService {
+
+    private final PriorityRepository repository; // сервис имеет право обращаться к репозиторию (БД)
+
+    public PriorityService(PriorityRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Priority> findAll(Long userId) {
+        return repository.findByUserIdOrderByIdAsc(userId);
+    }
+
+    public Priority add(Priority priority) {
+        return repository.save(priority); // метод save обновляет или создает новый объект, если его не было
+    }
+
+    public Priority update(Priority priority) {
+        return repository.save(priority); // метод save обновляет или создает новый объект, если его не было
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Priority findById(Long id) {
+        return repository.findById(id).get(); // т.к. возвращается Optional - можно получить объект методом get()
+    }
+
+    public List<Priority> find(String title, Long userId) {
+        return repository.findByTitle(title, userId);
+    }
+}
