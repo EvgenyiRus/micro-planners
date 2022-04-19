ce4431758c66bd1d373cdc88af101deb904795a4
Evgenyi Andreev <djonni-ussr@mail.ru>
2022-04-19 21:37:00 +0300

    feat: add @RefreshScope
    - for test use http://localhost:8765/planner-users/actuator/refresh
    - property in application.properties
     "# включение actuator
      management.endpoint.gateway.enabled=true
      # включение необходимых endpoints
      management.endpoints.web.exposure.include=*"

--- planner-todo/src/main/java/com/example/micro/planner/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/com/example/micro/planner/todo/PlannerTodoApplication.java
@@ -2,6 +2,7 @@ package com.example.micro.planner.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.example.micro.planner"}) // явное указание где искать bean для entities
 @EnableJpaRepositories(basePackages = {"com.example.micro.planner.todo"})
 @EnableFeignClients
+@RefreshScope
 public class PlannerTodoApplication {
 
     public static void main(String[] args) {
--- planner-users/src/main/java/com/example/micro/planner/users/PlannerUsersApplication.java
+++ planner-users/src/main/java/com/example/micro/planner/users/PlannerUsersApplication.java
@@ -2,6 +2,7 @@ package com.example.micro.planner.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableEurekaClient
 @ComponentScan(basePackages = {"com.example.micro.planner"})
 @EnableJpaRepositories(basePackages = {"com.example.micro.planner.users"})
+@RefreshScope
 public class PlannerUsersApplication {
 
     public static void main(String[] args) {
